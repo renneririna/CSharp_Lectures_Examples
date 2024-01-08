8f3be9dac0a6e5d84606ef40f951da353d6a8628
IrinaRenner <rennersha@mail.ru>
2024-01-08 15:15:15 +0300

    Добавлен 11-й проект Example011_ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,32 @@
+﻿// определяем массив из 10-ти элементов
+// конструкция new int[10] - создай новый массив из 10-ти элементов
+// по умолчание эти элементы заполняются нулями
+int [] array = new int[10];
+
+// вызываем методы
+FillArray(array); 
+PrintArray(array);
+
+// метод void - наполняет массив случайными числами
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+// метод void - печатает массив
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
